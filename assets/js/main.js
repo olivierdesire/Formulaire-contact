7d389aa4d4c268051547a5e47b1ffd1d842d9ebb
Olivier Désiré <olivierdesire@MacBook-Pro-de-Olivier.local>
2022-12-08 22:27:50 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("page chargée");
+
+  //   document.addEventListener("click", () => {
+  //     console.log("clicked");
+  //   });
+
+  document.querySelector("form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+    console.log("submit");
+
+    const firstname = document.querySelector("#firstname").value;
+    const lastname = document.querySelector("#lastname").value;
+    const email = document.querySelector("#email").value;
+    const subject = document.querySelector("#subject").value;
+    const message = document.querySelector("#message").value;
+
+    console.log("data >>>", {
+      firstname,
+      lastname,
+      email,
+      subject,
+      message,
+    });
+
+    try {
+      const response = await axios.post(
+        "https://site--backend-formulaire--97yqlpf4l44b.code.run/form",
+        {
+          firstname,
+          lastname,
+          email,
+          subject,
+          message,
+        }
+      );
+      console.log("response >>>>", response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
